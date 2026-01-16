e40111008bafd5ba04af1621b1b45a1664869b47
work <kondusov.ru@gmail.com>
2026-01-16 11:11:26 +0300

    добавили условие в баланс строки, проверяем - не закрылась ли скобка раньше чем открылась

--- exam/example/balance-string.js
+++ exam/example/balance-string.js
@@ -4,6 +4,7 @@ function balance(str){
     arr = str.split('');
     result = 0;
     for(key in arr){
+        if(result < 0) return 'Не сбалансирована - УШЛИ в МИНУС';
         let symbol = arr[key];
         if(symbol == '('){result+=1};
         if(symbol == ')') result-=1;
@@ -11,7 +12,7 @@ function balance(str){
     if(result == 0) return true;
     else return result;
 }
-let text = 'Первый метод — str.indexOf(substr, pos). Он ищет подстроку ';
+let text = 'Первый метод — str.indexOf)substr, pos(. Он ищет подстроку ';
 let bad_text = 'Первый метод — str.indexOf(substr, pos. Он ищет подстроку ';
 console.log(balance(text));
 console.log(balance(bad_text));
@@ -20,10 +21,11 @@ console.log(balance(bad_text));
 function Balanced(str) {
     let balanc = 0;
     for (char of str) {
-      if (char == '(') balanc++;
-      if (char == ')') balanc--;
+        if(balanc < 0) return 'balanc - УШЛЁЛ в МИНУС';
+        if (char == '(') balanc++;
+        if (char == ')') balanc--;
     }
     return balanc == 0;
   }
-  console.log(Balanced("(asdasd(sdsadas)asdasd)кеуекуекуе((фывфывфыв((ыфвфывф)))фывыфвыфв)"));
+  console.log(Balanced(")asdasd(sdsadas(asdasd)кеуекуекуе((фывфывфыв((ыфвфывф)))фывыфвыфв)"));
   //END альтернативная запись
